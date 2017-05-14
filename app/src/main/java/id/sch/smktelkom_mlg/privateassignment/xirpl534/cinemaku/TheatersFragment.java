f0c3c6d47d6d3fb7375fbdbe515f9bab352637fa
Mokleters <tiara.eka99@gmail.com>
2017-05-14 13:51:47 +0700

    Adding Cooming Soon Movie

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl534/cinemaku/TheatersFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl534/cinemaku/TheatersFragment.java
@@ -2,10 +2,26 @@ package id.sch.smktelkom_mlg.privateassignment.xirpl534.cinemaku;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.privateassignment.xirpl534.cinemaku.adapter.SourceAdapter;
+import id.sch.smktelkom_mlg.privateassignment.xirpl534.cinemaku.model.Result;
+import id.sch.smktelkom_mlg.privateassignment.xirpl534.cinemaku.model.SourcesResponse;
+import id.sch.smktelkom_mlg.privateassignment.xirpl534.cinemaku.service.GsonGetRequest;
+import id.sch.smktelkom_mlg.privateassignment.xirpl534.cinemaku.service.VolleySingleton;
 
 
 /**
@@ -13,6 +29,8 @@ import android.view.ViewGroup;
  */
 public class TheatersFragment extends Fragment {
 
+    ArrayList<Result> mList = new ArrayList<>();
+    SourceAdapter mAdapter;
 
     public TheatersFragment() {
         // Required empty public constructor
@@ -24,6 +42,45 @@ public class TheatersFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_theaters, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        RecyclerView recyclerView = (RecyclerView) getView().findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        mAdapter = new SourceAdapter(this.getActivity(), mList);
+        recyclerView.setAdapter(mAdapter);
+
+        downloadDataSources();
+
+    }
+
+    private void downloadDataSources() {
+        String url = "https://api.themoviedb.org/3/movie/upcoming?api_key=5c2be7f416c791e0302bcdf3c53ca3ac";
+
+        GsonGetRequest<SourcesResponse> myRequest = new GsonGetRequest<SourcesResponse>
+                (url, SourcesResponse.class, null, new Response.Listener<SourcesResponse>() {
+
+                    @Override
+                    public void onResponse(SourcesResponse response) {
+                        Log.d("FLOW", "onResponse: " + (new Gson().toJson(response)));
+
+                        mList.addAll(response.results);
+                        mAdapter.notifyDataSetChanged();
+
+                    }
+
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("FLOW", "onErrorResponse: ", error);
+                    }
+                });
+        VolleySingleton.getInstance(this.getActivity()).addToRequestQueue(myRequest);
     }
 
 }
